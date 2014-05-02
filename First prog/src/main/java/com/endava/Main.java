52e3757af313f67cf74013e985d2da47c1a512a1
catalincatana <catalin.catana3@gmail.com>
2014-05-02 15:43:55 +0300

    Vineri

---
+++ First prog/src/main/java/com/endava/Main.java
@@ -0,0 +1,36 @@
+package com.endava;
+
+/**
+ * Created by ccatana on 4/28/2014.
+ */
+public class Main {
+    public static void main(String[] args) {
+        int result = 1 ; // result is now 3
+        result += 2;
+        System.out.println(result);
+
+        result -= 1; // result is now 2
+        System.out.println(result);
+
+        result *= 2; // result is now 4
+        System.out.println(result);
+
+        result /= 2; // result is now 2
+        System.out.println(result);
+
+        result += 8; // result is now 10
+        result %= 7; // result is now 3
+        System.out.println(result);
+
+
+        int i = 3;
+        i++;
+        System.out.println(i);    // "4"
+        ++i;
+        System.out.println(i);    // "5"
+        System.out.println(++i);  // "6"
+        System.out.println(i++);  // "6"
+        System.out.println(i);    // "7"
+
+    }
+}
