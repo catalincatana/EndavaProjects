b78531da281c89fbee5509498b8b866b7931e35f
catalincatana <catalin.catana3@gmail.com>
2014-05-15 14:45:48 +0300

    WebFundamentals

---
+++ WebFundamentals/src/main/java/com/endava/PetStore.java
@@ -0,0 +1,37 @@
+package com.endava;
+
+
+ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+/**
+ *
+ * @author Georgiana Danila
+ *
+ */
+@JsonIgnoreProperties(ignoreUnknown=true)
+public class PetStore {
+    private Info info;
+    private String apiVersion;
+    private String swaggerVersion;
+
+    public Info getInfo() {
+        return info;
+    }
+    public void setInformation(Info info) {
+        this.info = info;
+    }
+    public String getApiVersion() {
+        return apiVersion;
+    }
+    public void setApiVersion(String apiVersion) {
+        this.apiVersion = apiVersion;
+    }
+    public String getSwaggerVersion() {
+        return swaggerVersion;
+    }
+    public void setSwaggerVersion(String swaggerVersion) {
+        this.swaggerVersion = swaggerVersion;
+    }
+
+
+}
