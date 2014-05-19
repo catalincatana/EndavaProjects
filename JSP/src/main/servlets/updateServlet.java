f4f25ed8235a17d7a8eb6047394ca3ab42497308
catalincatana <catalin.catana3@gmail.com>
2014-05-19 13:16:38 +0300

    Final

---
+++ JSP/src/main/servlets/updateServlet.java
@@ -0,0 +1,76 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Created by ccatana on 5/16/2014.
+ */
+public class UpdateServlet extends HttpServlet {
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        java.sql.Statement stmt = null;
+
+        String cod =request.getParameter("codDepartament");
+        String nume =request.getParameter("denumire");
+
+        response.setContentType("text/html");
+
+
+        // Actual logic goes here.
+        PrintWriter out = response.getWriter();
+       /* out.println("<h1>" + name + "</h1>");
+        out.println("<body>" + departament + "</body>");*/
+        out.println("Si acum conectarea:</br>");
+
+        Connection connection = getConnection();
+
+        String sql = "UPDATE departament SET Denumire_Departament='"+nume+"' WHERE Cod_Departament="+Integer.parseInt(cod)+"";
+
+        try {
+            stmt = connection.createStatement();
+            stmt.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        out.println("S-a efectuat si Stergerea!!");
+    }
+
+
+    public Connection getConnection(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Where is your MySQL JDBC Driver?");
+            e.printStackTrace();
+            return null;
+        }
+
+        System.out.println("MySQL JDBC Driver Registered!");
+        Connection connection = null;
+
+
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/endava", "root", "catamaster11");
+
+        } catch (SQLException e) {
+            System.out.println("Connection Failed! Check output console");
+            e.printStackTrace();
+            return null;
+        }
+
+        if (connection != null) {
+            System.out.println("You made it, take control your database now!");
+        } else {
+            System.out.println("Failed to make connection!");
+        }
+        return connection;
+    }
+}
